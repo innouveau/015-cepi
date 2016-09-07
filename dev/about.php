50b6baa4d474af9909cdc1d9ad0fb80799d33b4c
Jeroen Breen <jeroen@innouveau.nl>
2016-09-07 11:48:28 +0200

    style about page

--- dev/about.php
+++ dev/about.php
@@ -3,26 +3,37 @@
     <body>
 
         <div class="content">
-            <p>
+            <div class="col">
+                <img src="images/logos/cepi_zw.jpg"><br>
+                <img src="images/logos/reffibre_zw.jpg">
+            </div>
+
+            <div class="col">
+                <p>
                 This application is made on behalf of the
-                <a href="http://www.cepi.org/" target="_blank">Confederation of European Paper Industries</a>.
-                It is developed in collaboration by <a href="http://www.bumaga.nl/" target="_blank">Bumaga</a>
-                (research and content), <a href="http://www.info-graphics.nl/" target="_blank">Schwandt Infographics</a>
-                (concept and design) and <a href="http://www.innouveau.nl" target="_blank">Innouveau</a>
-                (development).
-            </p>
-            <p>
-                You can download the underlying report here:<br>
-                <a href="" target="_blank">Name report</a>
-            </p>
-            <p>
-                Logo Reffibre<br>
-                The research leading to these results has received funding from
-                the European Community's Seventh Framework Programme under grant
-                agreement n° 604187
-            </p>
-            <div class="content-footer">
-                <a href="index.php" target="_self" class="link">Back to infographic</a>
+                    <a href="http://www.cepi.org/" target="_blank">Confederation of European Paper Industries</a>.
+                </p>
+                <p>
+                    The research leading to these results has received funding from
+                    the European Community's Seventh Framework Programme under grant
+                    agreement n° 604187
+                </p>
+                <p>
+                    You can download the underlying report here:<br>
+                    <a href="" target="_blank">Name report</a>
+                </p>
+            </div>
+
+            <div class="col">
+                <p>
+                    The application is developed by <a href="http://www.bumaga.nl/" target="_blank">Bumaga</a>
+                    (research and content), <a href="http://www.info-graphics.nl/" target="_blank">Schwandt Infographics</a>
+                    (concept and design) and <a href="http://www.innouveau.nl" target="_blank">Innouveau</a>
+                    (development).
+                </p>
+                <div class="content-footer">
+                    <a href="index.php" target="_self" class="link">Back to infographic</a>
+                </div>
             </div>
         </div>
 
